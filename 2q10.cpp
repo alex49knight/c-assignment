2cf9ac5a400ec5432ff2dd49d2c9500ed8c05aa9
alex49knight <31305202+alex49knight@users.noreply.github.com>
2017-09-01 19:07:38 +0530

    Add files via upload

---
+++ 2q10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main() {
+float a,b,c,d,e,f,g,h;
+cout<<"mark 1 ";
+cin>>a ;
+cout<<"mark 2 ";
+cin>> b ;
+cout<<"mark 3 ";
+cin>>c ;
+cout<<"mark 4 ";
+cin>> d;
+cout<<"mark 5 ";
+cin>> e;
+f=(a+b+c+d+e)/5;
+g=a+b+c+d+e;
+h=(g/500)*100;
+cout<<"total "<<g;
+cout<<"average "<<f;
+cout<<"percentage "<<h;
+}
