fbe9d047f7d662f5ca6cb9bca2e662427a2318f5
alex49knight <31305202+alex49knight@users.noreply.github.com>
2017-09-01 19:10:51 +0530

    Add files via upload

---
+++ 2q11.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main () {
+float a,b,c,d;
+cout<< "P ";
+cin>> a;
+cout<<" R ";
+cin>> b;
+cout<<"T ";
+cin>> c;
+d=(a*b*c)/100;
+cout<<"simple intrest "<<d;
+}
